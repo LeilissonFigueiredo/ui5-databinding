6cd9721f153571956025d7d68d39d82980100325
LeilissonFigueiredo <leilisson.figueiredo@gmail.com>
2026-06-26 14:44:45 -0300

    Step 5: One Way Data Binding

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,12 +1,19 @@
 sap.ui.define([
-    "sap/ui/core/UIComponent"
-],(UiComponent)  => {
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/BindingMode"
+],(UiComponent,BindingMode)  => {
     "use strict";
 
     return UiComponent.extend("ui5.databinding.Component", {
         metedata: {
             interfaces: ["sap.ui.core.IAsyncContentCreation"],
             manifest: "json"
+        },
+
+        init(){
+            UiComponent.prototype.init.apply(this, arguments);
+
+            this.getModel()?.setDefaultBindingMode(BindingMode.OneWay);
         }
 
     })
