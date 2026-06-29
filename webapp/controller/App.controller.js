b316a40a1b57f9e620b1a832d14723b3ae54643f
LeilissonFigueiredo <leilisson.figueiredo@gmail.com>
2026-06-29 10:50:26 -0300

    Step 15: Aggregation Binding Using a Factory Function

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     "sap/m/library",
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/type/Currency"
-],(mobileLibrary,Controller, Currency) => {
+    "sap/ui/model/type/Currency",
+    "sap/m/ObjectAttribute"
+],(mobileLibrary,Controller, Currency, ObjectAttribute ) => {
     "use strict";
 
     const App = Controller.extend("ui5.databinding.controlle.App", {
@@ -32,6 +33,26 @@ sap.ui.define([
                 "model": "products"
             })
 
+        },
+        
+        productListFactory(sId, oContext){
+            let oUIControl;
+
+            //Decide based on the data which dependent to clone
+            if (oContext.getProperty("UnitsInStock") === 0 && oContext.getProperty("Discontinued")){
+                // The item is discontinued, so use a StandardListItem
+                oUIControl = this.byId("sliProductSimple").clone(sId);
+            }else{
+                oUIControl = this.byId("sliProductExtended").clone(sId);
+            
+                if (oContext.getProperty("UnitsInStock") < 1 ){
+                    oUIControl.addAttribute(new ObjectAttribute({
+                        "text": "{i18n>outOfStock}"
+                    }))
+                }
+            }
+
+            return oUIControl;
         }
 
     });
